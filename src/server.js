57e965b652e37e825036d99e177e918a2daaeb4f
zineb bennani <zineb.bennanigabsi@gmail.com>
2026-09-24 15:08:21 +0000

    ajouter le premier workflow CI

--- src/server.js
+++ src/server.js
@@ -3,5 +3,8 @@ const app = require("./app");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`CI/CD Demo S2 démarrée sur le port ${port}`);
+  console.log(`CI/CD Demo S2 démarrée sur le port ${port} ,
+               http://localhost:${port}/api/calcul/ttc?prix=100&tva=20 ,
+               http://localhost:${port}/api/calcul/health
+    `);
 });
