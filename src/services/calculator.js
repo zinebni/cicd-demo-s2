6025f10defa1d26f95315c25b4011471a1cf8177
zineb bennani <zineb.bennanigabsi@gmail.com>
2026-09-24 15:34:31 +0000

    erreur de syntaxe  prixHT * (1 + tauxTVA / 120)

--- src/services/calculator.js
+++ src/services/calculator.js
@@ -1,5 +1,5 @@
 function addition(a, b) {
-  return a - b;
+  return a + b;
   // return a + b;
 }
 
@@ -12,7 +12,7 @@ function calculerTTC(prixHT, tauxTVA) {
     throw new RangeError("prixHT et tauxTVA doivent être positifs");
   }
 
-  return Number((prixHT * (1 + tauxTVA / 100)).toFixed(2));
+  return Number((prixHT * (1 + tauxTVA / 120)).toFixed(2));
 }
 
 module.exports = { addition, calculerTTC };
